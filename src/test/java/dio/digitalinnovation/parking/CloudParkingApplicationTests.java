87ee5bd5b9e871d490839bdf115e0e02663f1baf
Rui Palmeira Reis Junior <ruiprj@gmail.com>
2022-11-07 10:23:31 -0400

    Estrutura inicial

---
+++ src/test/java/dio/digitalinnovation/parking/CloudParkingApplicationTests.java
@@ -0,0 +1,13 @@
+package dio.digitalinnovation.parking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudParkingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
